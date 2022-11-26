84c9c8e5f5f831e695b95b157f15e504596acc17
amaribel <cereviziae@mail.ru>
2022-11-27 00:14:58 +0300

    ready

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,20 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(cat.getName());
+        System.out.println(cat2.getName());
+
+        boolean isEqual = bean == bean2;
+        boolean isEqual2 = cat == cat2;
+
+        System.out.println("bean and bean2 are equal - " + isEqual);
+        System.out.println("cat and cat2 are equal - " + isEqual2);
+
     }
 }
